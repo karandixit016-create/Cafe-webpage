c925070d4ceb50e20c30c617855bc8a10550ff4d
Karan Dixit <karandixit016@gmail.com>
2026-07-17 00:52:36 +0530

    Update coffee webpage and fix deployment config

--- src/component/MovingBanner.jsx
+++ src/component/MovingBanner.jsx
@@ -1,1 +1,33 @@
-i
+import {motion} from 'framer-motion'
+
+
+const MovingBanner = () => {
+  return (
+    <div className='overflow-hidden bg-stone-700 py-5'>
+        <motion.div 
+          animate={{x: ["0%" , "-50%"]}}
+          transition={{
+                repeat:Infinity,
+                ease:"linear",
+                duration:20
+          }}
+          className='flex whitespace-nowrap gap-12 text-white text-lg font-medium'
+          >
+          {Array.from({ length: 2 }).map((_, index) => (
+             <div key={index} className="flex gap-12">
+               <span>☕ Freshly Brewed</span>
+               <span>●</span>
+               <span>Kulhad Chai</span>
+               <span>●</span>
+               <span>Authentic Taste</span>
+               <span>●</span>
+               <span>Since 1998</span>
+               <span>●</span>
+             </div>
+           ))}
+       </motion.div>
+    </div>
+  )
+}
+
+export default MovingBanner
