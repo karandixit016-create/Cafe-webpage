84e0d2d13dd76d9045c3b3f5696cfcd5129541f5
Karan Dixit <karandixit016@gmail.com>
2026-07-17 14:54:28 +0530

    flex vite base path for github and vercel

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: mode === "production" ? "/Cafe-webpage/" : "/",
-}));
+  // eslint-disable-next-line no-undef
+  base: process.env.GITHUB_ACTIONS ? "/Cafe-webpage/" : "/",
+});
